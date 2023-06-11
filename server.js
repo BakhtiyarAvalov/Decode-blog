40cd9ecbe860f7ad271b950ed4222db2047d0c5c
BakhtiyarAvalov <Avalov90@mail.ru>
2023-06-11 19:53:05 +0600

    add index.js

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'))
+app.set ("view engine", "ejs")
+
+app.get('/', (req, res) =>{
+    res.render("index.ejs")
+})
+
+app.get('/login', (req, res)=>{
+    res.render("login.ejs")
+})
+
+app.get('/register', (req, res)=>{
+    res.render("register.ejs")
+})
+
+app.get('/profile', (req, res)=>{
+    res.render("profile.ejs")
+})
+
+app.get('/admin', (req, res)=>{
+    res.render("adminProfile.ejs")
+})
+
+app.get('/new', (req, res)=>{
+    res.render("newBlog.ejs")
+})
+
+app.get('/edit', (req, res)=>{
+    res.render("editBlog.ejs")
+})
+
+app.get('/mainPage', (req, res)=>{
+    res.render("mainPage.ejs")
+})
+
+app.get('/user', (req, res)=>{
+    res.render("users.ejs")
+})
+
+const PORT = 8001;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
